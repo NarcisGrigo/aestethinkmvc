f15ece4a0edd175bb52df0e3eb190e42851f6190
NarcisGrigo <132442585+NarcisGrigo@users.noreply.github.com>
2024-01-24 16:48:29 +0100

    edited collections and added a loop

--- views/collections.php
+++ views/collections.php
@@ -13,81 +13,44 @@ require_once("../public/header.html.php");
             <input type="submit" class="logout custom-button" value="Logout"
                 style="background-color: #1b201e; color:#ffd700; width:120px; height:35px; font-family: Raleway, sans-serif; font-size:25px; border:none; outline:none; box-shadow:none; cursor:pointer;">
         </form>
-        <!-- <div id="carouselExampleIndicators" class="carousel slide">
-            <div class="carousel-inner">
-                <div class="carousel-item active"> -->
-        <h2 class="icarus_title">Icarus Collection</h2>
-        <div class="tattoo">
-            <div class="card">
-                <img src="/aesthetinkmvc/public/assets/img/icarus_to_the_sun.jpg" class="card-img-top" alt="..." />
-                <div class="card-body">
-                    <p class="titletattoo">Tattoo Model</p>
-                    <h5 class="card-title">Icarus To the Sun</h5>
-                    <p class="price">€350</p>
-                    <button type="submit" class="buy-button">Buy</button>
-                </div>
-            </div>
-            <div class="card">
-                <img src="/aesthetinkmvc/public/assets/img/winged_icarus.jpg" class="card-img-top" alt="..." />
-                <div class="card-body">
-                    <p class="titletattoo">Tattoo Model</p>
-                    <h5 class="card-title">Winged Icarus</h5>
-                    <p class="price">€500</p>
-                    <button type="submit" class="buy-button">Buy</button>
-                </div>
-            </div>
-            <div class="card">
-                <img src="/aesthetinkmvc/public/assets/img/icarus_in_the_sun.jpg" class="card-img-top" alt="..." />
-                <div class="card-body">
-                    <p class="titletattoo">Tattoo Model</p>
-                    <h5 class="card-title">Icarus In The Sun</h5>
-                    <p class="price">€550</p>
-                    <button type="submit" class="buy-button">Buy</button>
-                </div>
-            </div>
-        </div>
+
+        <?php
+        $collections = [
+            'Icarus Collection' => [
+                ['name' => 'Icarus To the Sun', 'image' => '/aesthetinkmvc/public/assets/img/icarus_to_the_sun.jpg', 'price' => '€350'],
+                ['name' => 'Winged Icarus', 'image' => '/aesthetinkmvc/public/assets/img/winged_icarus.jpg', 'price' => '€500'],
+                ['name' => 'Icarus In The Sun', 'image' => '/aesthetinkmvc/public/assets/img/icarus_in_the_sun.jpg', 'price' => '€550'],
+            ],
+            'Sculpture Collection' => [
+                ['name' => 'Ingenious Sculpture', 'image' => '/aesthetinkmvc/public/assets/img/ingenious.jpg', 'price' => '€550'],
+                ['name' => 'Centaur Sculpture', 'image' => '/aesthetinkmvc/public/assets/img/centaur.jpg', 'price' => '€600'],
+                ['name' => 'Poseidon Sculpture', 'image' => '/aesthetinkmvc/public/assets/img/poseidon.jpg', 'price' => '€500'],
+            ],
+        ];
+
+        foreach ($collections as $collectionTitle => $items) {
+            echo '<div class="carousel-item">';
+            echo '<h2 class="icarus_title">' . $collectionTitle . '</h2>';
+            echo '<div class="tattoo">';
+            foreach ($items as $item) {
+                echo '<div class="card">';
+                echo '<img src="' . $item['image'] . '" class="card-img-top" alt="..." />';
+                echo '<div class="card-body">';
+                echo '<p class="titletattoo">Tattoo Model</p>';
+                echo '<h5 class="card-title">' . $item['name'] . '</h5>';
+                echo '<p class="price">' . $item['price'] . '</p>';
+                echo '<button type="submit" class="buy-button">Buy</button>';
+                echo '</div>';
+                echo '</div>';
+            }
+            echo '</div>';
+            echo '</div>';
+        }
+        ?>
     </div>
-    <div class="carousel-item">
-        <h2 class="icarus_title">Sculpture Collection</h2>
-        <div class="tattoo">
-            <div class="card">
-                <img src="/aesthetinkmvc/public/assets/img/ingenious.jpg" class="card-img-top" alt="..." />
-                <div class="card-body">
-                    <p class="titletattoo">Tattoo Model</p>
-                    <h5 class="card-title">Ingenious Sculpture</h5>
-                    <p class="price">€550</p>
-                    <button type="submit" class="buy-button">Buy</button>
-                </div>
-            </div>
-            <div class="card">
-                <img src="/aesthetinkmvc/public/assets/img/centaur.jpg" class="card-img-top" alt="..." />
-                <div class="card-body">
-                    <p class="titletattoo">Tattoo Model</p>
-                    <h5 class="card-title">Centaur Sculpture</h5>
-                    <p class="price">€600</p>
-                    <button type="submit" class="buy-button">Buy</button>
-                </div>
-            </div>
-            <div class="card">
-                <img src="/aesthetinkmvc/public/assets/img/poseidon.jpg" class="card-img-top" alt="..." />
-                <div class="card-body">
-                    <p class="titletattoo">Tattoo Model</p>
-                    <h5 class="card-title">Poseidon Sculpture</h5>
-                    <p class="price">€500</p>
-                    <button type="submit" class="buy-button">Buy</button>
-                </div>
-            </div>
-        </div>
-    </div>
-    </div>
-    </div>
-    </div>
+
+    <?php
+    require_once("../public/footer.html.php");
+    ?>
 </body>
-<?php
-require_once("../public/footer.html.php");
-?>
-<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-    crossorigin="anonymous"></script>
-<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script> -->
+</html>
